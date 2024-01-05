eb055a0fbf1327a3a1efafd2278ff3a71aa260d4
Casper Juel <Casper-juel@hotmail.com>
2024-01-05 23:05:54 +0100

    Work and implement solutions to problems

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
+const reverseString = function(myInput) {
+    original = myInput;
+    copy = [];
+    copyString = "";
 
+    for (let i = 0; i < myInput.length; i++) {
+        copy[i] = original[(myInput.length - 1 - i)]
+    }
+    
+    let i = 0;
+    while (i < copy.length) {
+        copyString += copy[i]
+        i++;
+    }
+    return copyString;
 };
+
+console.log(reverseString(''));
 
 // Do not edit below this line
 module.exports = reverseString;
