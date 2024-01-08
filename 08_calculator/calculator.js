fd864ae3cf87d0ea4471911596a4932e5fb7babc
Casper Juel <Casper-juel@hotmail.com>
2024-01-08 15:04:13 +0100

    Add solution attempts

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(firstNum, secondNum) {
+	return firstNum + secondNum;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNum, secondNum) {
+	return firstNum - secondNum;
 };
 
-const sum = function() {
-	
+const sum = function(number) {
+  let inSum = 0;
+  number.forEach(elem => {
+    inSum += elem
+  });
+  return inSum
 };
 
-const multiply = function() {
-
+const multiply = function(number) {
+  let inSum = 1;
+  number.forEach(elem => {
+    inSum *= elem
+  });
+  return inSum
 };
 
-const power = function() {
-	
+const power = function(aP, bP) {
+	return aP ** bP;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let sum = 1;
+  if(number != 0) {
+    for (let i = 1; i < (number + 1) ; i++) {
+      sum = sum * i;
+  }
+  } else {
+    return 1;
+  }
+  return sum
 };
 
 // Do not edit below this line
